421b5cc4c053e6964c5b8293afb49ff1174856d2
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-13 05:06:44 -0400

    chore: added some example variable annotations

---
+++ features/annotations/variables.ts
@@ -0,0 +1,22 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let now: Date = new Date();
+
+// Arrays
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+
+// Classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
