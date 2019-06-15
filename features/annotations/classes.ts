159d0150f461a06da2d3a7e46cda0d07183dc1f2
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-15 19:31:21 -0400

    docs: tried to add documentation

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -1,28 +1,64 @@
+/** Class representing a vehicle */
 class Vehicle {
+  /**
+   * Vehicle color
+   * @public
+   */
   public color: string;
 
+  /**
+   * Number of wheels
+   * @public
+   */
+  public wheels: number = 2;
+
+  /**
+   * Contruct a Vehicle
+   * @param {string} color color of the car
+   */
   constructor(color: string) {
     this.color = color;
   }
 
+  /**
+   * logs the sound the vehicle makes
+   *
+   * this method is protected meaning its only accessable by itself or child
+   * classes
+   *
+   * @param message string to print
+   */
   protected drive(message: string = 'chugga chugga'): void {
     console.log(message);
   }
 
+  /**
+   * Logs a honk sound
+   */
   public honk(): void {
     console.log('beep');
   }
 }
 
+/**
+ * Class representing a car
+ * @extends Vehicle
+ */
 class Car extends Vehicle {
-  public wheels: number;
+  public wheels = 4;
 
-  constructor(color: string, wheels: number = 4) {
+  /**
+   * Construct the Car Class
+   * @param {string} color Color of the car
+   * @param {number} wheels How many wheels? (optional)
+   */
+  constructor(color: string) {
     super(color);
-
-    this.wheels = wheels;
   }
 
+  /**
+   * Start the driving process
+   */
   public startDrivingProcess(): void {
     this.drive('vroom');
     this.honk();
