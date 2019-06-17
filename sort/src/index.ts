8d04c278af53a925b56afb6bbb5f774e27e77537
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-17 12:35:35 -0400

    chore: setup array number sort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,26 @@
-console.log('hi there')
+class Sorter {
+  collection: number[]
 
-const logSomething = () => {
-  console.log('something')
+  constructor(collection: number[]) {
+    this.collection = collection
+  }
+
+  sort(): void {
+    const { length }: { length: number } = this.collection
+
+    for (let i: number = 0; i < length; i += 1) {
+      for (let j: number = 0; j < length - i - 1; j += 1) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const lefthand = this.collection[j]
+          this.collection[j] = this.collection[j + 1]
+          this.collection[j + 1] = lefthand
+        }
+      }
+    }
+  }
 }
+
+const sorter: Sorter = new Sorter([10, 3, -5, 0])
+
+sorter.sort()
+console.log(sorter.collection)
